f9b2d0c8a06b804163c5a2e1f6ba13ffee238652
Barney Boisvert <bboisvert@armus.com>
2021-11-10 19:13:39 -0800

    part two

--- src/main/kotlin/aoc2019/day01_rocket_equation/RocketEquation.kt
+++ src/main/kotlin/aoc2019/day01_rocket_equation/RocketEquation.kt
@@ -4,13 +4,29 @@ import kotlin.math.max
 
 fun main() {
     util.solve(3336439, ::partOne)
-//    util.solve(6, ::partTwo)
+    util.solve(5001791, ::partTwo)
 }
+
+fun fuelForMass(m: Int) =
+    max(0, m / 3 - 2)
 
 fun partOne(input: String) = input
     .lines()
     .map(String::toInt)
-    .sumOf { max(0, it / 3 - 2) }
+    .sumOf(::fuelForMass)
 
-fun partTwo(input: String) = input.trim().length
+fun partTwo(input: String): Int {
+    val result = mutableListOf<Int>()
+    input
+        .lines()
+        .map(String::toInt)
+        .forEach {
+            var n = it
+            while (n > 0) {
+                n = fuelForMass(n)
+                result.add(n)
+            }
+        }
+    return result.sum()
+}
 
