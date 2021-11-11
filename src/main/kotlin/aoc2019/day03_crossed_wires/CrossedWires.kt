08cad7206812a90fff4121d33aaf4813f58d5382
Barney Boisvert <bboisvert@armus.com>
2021-11-11 07:05:37 -0800

    part one

--- src/main/kotlin/aoc2019/day03_crossed_wires/CrossedWires.kt
+++ src/main/kotlin/aoc2019/day03_crossed_wires/CrossedWires.kt
@@ -1,11 +1,54 @@
 package aoc2019.day03_crossed_wires
 
+import geom2d.Dir
+import geom2d.Point
+import geom2d.Step
+
 fun main() {
-    util.solve(10, ::partOne)
+    util.solve(1084, ::partOne)
 //    util.solve(6, ::partTwo)
 }
 
-fun partOne(input: String) = input.length
+private fun String.toStep() =
+    Step(
+        when (this.first()) {
+            'U' -> Dir.NORTH
+            'R' -> Dir.EAST
+            'D' -> Dir.SOUTH
+            'L' -> Dir.WEST
+            else -> throw IllegalArgumentException("Unknown '${this.first()}' step direction")
+        },
+        this.substring(1).toLong()
+    )
+
+private fun String.toSteps() =
+    this.trim()
+        .split(",")
+        .map(String::toStep)
+
+private fun walkForPositions(steps: List<Step>): Set<Point> {
+    // deliberately not including origin, even though the wire starts there,
+    // as the theme is "crossed wires" and the shared origin appears like a
+    // cross, even though it isn't. So just ignore it here. Fingers crossed.
+    val points = mutableSetOf<Point>()
+    steps.fold(Point(0, 0)) { base, s ->
+        (0 until s.n).fold(base) { curr, _ ->
+            val next = curr.step(s.dir)
+            points += next
+            next
+        }
+    }
+    return points
+}
+
+fun partOne(input: String): Long {
+    val (a, b) = input
+        .lines()
+        .map(String::toSteps)
+        .map(::walkForPositions)
+    return a.intersect(b)
+        .minOf(Point::mannDist)
+}
 
 fun partTwo(input: String) = input.trim().length
 
--- src/test/kotlin/aoc2019/day03_crossed_wires/CrossedWiresKtTest.kt
+++ src/test/kotlin/aoc2019/day03_crossed_wires/CrossedWiresKtTest.kt
@@ -7,9 +7,30 @@ internal class CrossedWiresKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(1, partOne("a"))
-        assertEquals(3, partOne(" a "))
-        assertEquals(3, partOne("cat"))
+        assertEquals(
+            6, partOne(
+                """
+            R8,U5,L5,D3
+            U7,R6,D4,L4
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            159, partOne(
+                """
+            R75,D30,R83,U83,L12,D49,R71,U7,L72
+            U62,R66,U55,R34,D71,R55,D58,R83
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            135, partOne(
+                """
+            R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+            U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+        """.trimIndent()
+            )
+        )
     }
 
     @Test
