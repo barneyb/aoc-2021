ecf6e5ec6ed113f5fee06f3d6265782ec2f01a68
Barney Boisvert <bboisvert@armus.com>
2021-11-12 16:24:18 -0800

    skeleton

---
+++ src/main/kotlin/aoc2015/day03_spherical_houses/SphericalHouses.kt
@@ -0,0 +1,11 @@
+package aoc2015.day03_spherical_houses
+
+fun main() {
+    util.solve(::partOne)
+//    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
+
---
+++ src/test/kotlin/aoc2015/day03_spherical_houses/SphericalHousesKtTest.kt
@@ -0,0 +1,20 @@
+package aoc2015.day03_spherical_houses
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class SphericalHousesKtTest {
+
+    @Test
+    fun partOne() {
+        assertEquals(-1, partOne("input"))
+    }
+
+    @kotlin.test.Ignore // todo: reinstate when ready!
+    @Test
+    fun partTwo() {
+        assertEquals(-1, partTwo("input"))
+    }
+
+}
+
