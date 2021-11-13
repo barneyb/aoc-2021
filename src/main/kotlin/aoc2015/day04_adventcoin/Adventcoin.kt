31234fdbf4485cf065f06f3c28788db43b337393
Barney Boisvert <bboisvert@armus.com>
2021-11-12 16:59:46 -0800

    skeleton

---
+++ src/main/kotlin/aoc2015/day04_adventcoin/Adventcoin.kt
@@ -0,0 +1,11 @@
+package aoc2015.day04_adventcoin
+
+fun main() {
+    util.solve(::partOne)
+    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
+
---
+++ src/test/kotlin/aoc2015/day04_adventcoin/AdventcoinKtTest.kt
@@ -0,0 +1,20 @@
+package aoc2015.day04_adventcoin
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class AdventcoinKtTest {
+
+    @Test
+    fun partOne() {
+        assertEquals(-1, partOne("input"))
+    }
+
+    @kotlin.test.Ignore // todo: reinstate when ready!
+    @Test
+    fun partTwo() {
+        assertEquals(-1, partTwo("input"))
+    }
+
+}
+
