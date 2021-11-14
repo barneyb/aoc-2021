b40ce6c132f43da3a4f8d90d865d365e9c2f6d52
Barney Boisvert <bboisvert@armus.com>
2021-11-14 11:05:19 -0800

    skeleton

---
+++ src/main/kotlin/aoc2018/day02_inventory_management/InventoryManagement.kt
@@ -0,0 +1,11 @@
+package aoc2018.day02_inventory_management
+
+fun main() {
+    util.solve(::partOne)
+    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
+
---
+++ src/test/kotlin/aoc2018/day02_inventory_management/InventoryManagementKtTest.kt
@@ -0,0 +1,20 @@
+package aoc2018.day02_inventory_management
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class InventoryManagementKtTest {
+
+    @Test
+    fun partOne() {
+        assertEquals(-1, partOne("input"))
+    }
+
+    @kotlin.test.Ignore // todo: reinstate when ready!
+    @Test
+    fun partTwo() {
+        assertEquals(-1, partTwo("input"))
+    }
+
+}
+
