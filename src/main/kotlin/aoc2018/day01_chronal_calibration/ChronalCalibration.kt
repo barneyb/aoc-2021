2aa5e4e91c59b3c92f324466a2560f04cf78a56e
Barney Boisvert <bboisvert@armus.com>
2021-11-14 10:50:19 -0800

    part one

--- src/main/kotlin/aoc2018/day01_chronal_calibration/ChronalCalibration.kt
+++ src/main/kotlin/aoc2018/day01_chronal_calibration/ChronalCalibration.kt
@@ -1,11 +1,13 @@
 package aoc2018.day01_chronal_calibration
 
 fun main() {
-    util.solve(::partOne)
+    util.solve(513, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+fun partOne(input: String) = input
+    .lines()
+    .sumOf { it.trim().toInt() }
 
 fun partTwo(input: String) = input.trim().length
 
--- src/test/kotlin/aoc2018/day01_chronal_calibration/ChronalCalibrationKtTest.kt
+++ src/test/kotlin/aoc2018/day01_chronal_calibration/ChronalCalibrationKtTest.kt
@@ -2,12 +2,22 @@ package aoc2018.day01_chronal_calibration
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import util.cleanInput
 
 internal class ChronalCalibrationKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(-1, partOne("input"))
+        assertEquals(
+            3, partOne(
+                """
+            +1
+            -2
+            +3
+            +1
+        """.cleanInput()
+            )
+        )
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
