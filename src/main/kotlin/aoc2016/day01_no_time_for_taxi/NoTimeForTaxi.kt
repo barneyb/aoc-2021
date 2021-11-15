0a55c82300afb2970fb3cebe721e71c56b59c67b
Barney Boisvert <bboisvert@armus.com>
2021-11-14 20:39:04 -0800

    skeleton

---
+++ src/main/kotlin/aoc2016/day01_no_time_for_taxi/NoTimeForTaxi.kt
@@ -0,0 +1,10 @@
+package aoc2016.day01_no_time_for_taxi
+
+fun main() {
+    util.solve(::partOne)
+    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
---
+++ src/test/kotlin/aoc2016/day01_no_time_for_taxi/NoTimeForTaxiKtTest.kt
@@ -0,0 +1,19 @@
+package aoc2016.day01_no_time_for_taxi
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class NoTimeForTaxiKtTest {
+
+    @Test
+    fun partOne() {
+        assertEquals(0, partOne("input"))
+    }
+
+    @kotlin.test.Ignore // todo: reinstate when ready!
+    @Test
+    fun partTwo() {
+        assertEquals(0, partTwo("input"))
+    }
+
+}
