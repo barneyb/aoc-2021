42a0c866235bf3dd72f5ef93c6257da020237611
Barney Boisvert <bboisvert@armus.com>
2021-11-18 15:31:54 -0800

    part one

--- src/main/kotlin/aoc2017/day01_inverse_captcha/InverseCaptcha.kt
+++ src/main/kotlin/aoc2017/day01_inverse_captcha/InverseCaptcha.kt
@@ -1,10 +1,20 @@
 package aoc2017.day01_inverse_captcha
 
 fun main() {
-    util.solve(::partOne)
+    util.solve(1089, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+fun partOne(input: String): Int {
+    var prev = input[input.length - 1]
+    var sum = 0
+    for (c in input) {
+        if (c == prev) {
+            sum += c.digitToInt()
+        }
+        prev = c
+    }
+    return sum
+}
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/aoc2017/day01_inverse_captcha/InverseCaptchaKtTest.kt
+++ src/test/kotlin/aoc2017/day01_inverse_captcha/InverseCaptchaKtTest.kt
@@ -7,7 +7,10 @@ internal class InverseCaptchaKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(0, partOne("input"))
+        assertEquals(3, partOne("1122"))
+        assertEquals(4, partOne("1111"))
+        assertEquals(0, partOne("1234"))
+        assertEquals(9, partOne("91212129"))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
