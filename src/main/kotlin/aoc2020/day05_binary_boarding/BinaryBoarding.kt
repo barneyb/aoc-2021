905668ce17207fb00849bca9d4740a2edbc750dd
Barney Boisvert <bboisvert@armus.com>
2021-11-26 18:15:34 -0800

    skeleton

---
+++ src/main/kotlin/aoc2020/day05_binary_boarding/BinaryBoarding.kt
@@ -0,0 +1,10 @@
+package aoc2020.day05_binary_boarding
+
+fun main() {
+    util.solve(::partOne)
+    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
