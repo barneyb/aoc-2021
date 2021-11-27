347eac0a60088a41fc8692a73d3a85304512490b
Barney Boisvert <bboisvert@armus.com>
2021-11-26 17:18:46 -0800

    part one

--- src/main/kotlin/aoc2020/day04_passport_processing/PassportProcessing.kt
+++ src/main/kotlin/aoc2020/day04_passport_processing/PassportProcessing.kt
@@ -1,10 +1,27 @@
 package aoc2020.day04_passport_processing
 
+import util.paragraphs
+
 fun main() {
-    util.solve(::partOne)
+    util.solve(206, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+private val requiredKeys =
+    setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+
+fun partOne(input: String) =
+    input
+        .paragraphs()
+        .map {
+            it.split(' ')
+                .map { it.split(':') }
+                .associate { (a, b) -> Pair(a, b) }
+        }
+        .count { p ->
+            requiredKeys.all { k ->
+                p.containsKey(k)
+            }
+        }
 
 fun partTwo(input: String) = input.trim().length
