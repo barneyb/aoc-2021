d0632955b5fb04d769211ddcb2a64ec1a8679cc0
Barney Boisvert <bboisvert@armus.com>
2021-11-28 15:45:26 -0800

    part one

--- src/main/kotlin/aoc2019/day02_program_alarm/ProgramAlarm.kt
+++ src/main/kotlin/aoc2019/day02_program_alarm/ProgramAlarm.kt
@@ -1,10 +1,47 @@
 package aoc2019.day02_program_alarm
 
 fun main() {
-    util.solve(::partOne)
+    util.solve(4090689, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+private fun String.compile() =
+    trim()
+        .split(",")
+        .map { it.toInt() }
+        .toIntArray()
+
+fun partOne(input: String): Int {
+    val codes = input.compile()
+    codes[1] = 12
+    codes[2] = 2
+    return evaluateForPosition(codes, 0)
+}
+
+internal fun evaluateForPosition(program: String, position: Int) =
+    evaluateForPosition(program.compile(), position)
+
+internal fun evaluateForPosition(program: IntArray, position: Int): Int {
+    var ip = 0
+    while (true) {
+        // short-circuit HALT
+        if (program[ip] == 99) {
+            return program[position]
+        }
+        val a = program[program[ip + 1]]
+        val b = program[program[ip + 2]]
+        val destIdx = program[ip + 3]
+        when (program[ip]) {
+            1 -> { // ADD
+                program[destIdx] = a + b
+            }
+            2 -> { // MULT
+                program[destIdx] = a * b
+            }
+            else -> throw IllegalStateException("Unknown ${program[ip]} opcode at position $ip")
+        }
+        ip += 4
+    }
+}
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/aoc2019/day02_program_alarm/ProgramAlarmKtTest.kt
+++ src/test/kotlin/aoc2019/day02_program_alarm/ProgramAlarmKtTest.kt
@@ -11,7 +11,18 @@ internal class ProgramAlarmKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+        assertEquals(
+            70,
+            evaluateForPosition("1,9,10,3,2,3,11,0,99,30,40,50", 3)
+        )
+        assertEquals(
+            3500,
+            evaluateForPosition("1,9,10,3,2,3,11,0,99,30,40,50", 0)
+        )
+        assertEquals(2, evaluateForPosition("1,0,0,0,99", 0))
+        assertEquals(6, evaluateForPosition("2,3,0,3,99", 3))
+        assertEquals(9801, evaluateForPosition("2,4,4,5,99,0", 5))
+        assertEquals(30, evaluateForPosition("1,1,1,4,99,5,6,0,99", 0))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
