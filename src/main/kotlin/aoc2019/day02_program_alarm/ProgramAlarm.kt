27332bdf42e9c5c4b29d6f9a81cdc119ff645615
Barney Boisvert <bboisvert@armus.com>
2021-11-28 15:23:19 -0800

    skeleton

---
+++ src/main/kotlin/aoc2019/day02_program_alarm/ProgramAlarm.kt
@@ -0,0 +1,10 @@
+package aoc2019.day02_program_alarm
+
+fun main() {
+    util.solve(::partOne)
+    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
---
+++ src/test/kotlin/aoc2019/day02_program_alarm/ProgramAlarmKtTest.kt
@@ -0,0 +1,23 @@
+package aoc2019.day02_program_alarm
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+private val WORKED_EXAMPLE = """
+    input
+""".trimIndent()
+
+internal class ProgramAlarmKtTest {
+
+    @Test
+    fun partOne() {
+        assertEquals(0, partOne(WORKED_EXAMPLE))
+    }
+
+    @kotlin.test.Ignore // todo: reinstate when ready!
+    @Test
+    fun partTwo() {
+        assertEquals(0, partTwo(WORKED_EXAMPLE))
+    }
+
+}
