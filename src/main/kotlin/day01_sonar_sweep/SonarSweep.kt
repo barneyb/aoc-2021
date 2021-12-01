bcc0d85f66bb8634b5defa2c50ce3d6ed417b259
Barney Boisvert <bboisvert@armus.com>
2021-12-01 07:48:42 -0800

    part one

--- src/main/kotlin/day01_sonar_sweep/SonarSweep.kt
+++ src/main/kotlin/day01_sonar_sweep/SonarSweep.kt
@@ -1,10 +1,18 @@
 package day01_sonar_sweep
 
 fun main() {
-    util.solve(::partOne)
+    util.solve(1616, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+fun partOne(input: String) =
+    input
+        .lines()
+        .asSequence()
+        .map { it.toInt() }
+        .fold(Pair(0, Int.MAX_VALUE)) { (n, prev), it ->
+            Pair(if (it > prev) n + 1 else n, it)
+        }
+        .first
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/day01_sonar_sweep/SonarSweepKtTest.kt
+++ src/test/kotlin/day01_sonar_sweep/SonarSweepKtTest.kt
@@ -4,14 +4,23 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 private val WORKED_EXAMPLE = """
-    input
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
 """.trimIndent()
 
 internal class SonarSweepKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+        assertEquals(7, partOne(WORKED_EXAMPLE))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
