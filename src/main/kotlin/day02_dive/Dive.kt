2015a8f57b01aba91687e2ed74e1a62770f69c9c
Barney Boisvert <bboisvert@armus.com>
2021-12-01 22:01:35 -0800

    reimplement with some data classes

--- src/main/kotlin/day02_dive/Dive.kt
+++ src/main/kotlin/day02_dive/Dive.kt
@@ -5,38 +5,69 @@ fun main() {
     util.solve(1727785422, ::partTwo)
 }
 
-fun partOne(input: String): Int {
-    val dest = input
-        .lineSequence()
-        .fold(Pair(0, 0)) { p, line ->
-            val words = line.split(" ")
-            val n = words[1].toInt()
-            when (words[0]) {
-                "forward" -> Pair(p.first + n, p.second)
-                "up" -> Pair(p.first, p.second - n)
-                "down" -> Pair(p.first, p.second + n)
-                else -> throw IllegalArgumentException("Unrecognized '$line' instruction")
-            }
-        }
-    return dest.first * dest.second
+interface Sub<T : Sub<T>> {
+    val pos: Int
+    val depth: Int
+    fun forward(n: Int): T
+    fun up(n: Int): T
+    fun down(n: Int): T
+
+    val finalDestination
+        get() = pos * depth
 }
 
-fun partTwo(input: String): Int {
-    val dest = input
-        .lineSequence()
-        .fold(Triple(0, 0, 0)) { t, line ->
-            val words = line.split(" ")
-            val n = words[1].toInt()
-            when (words[0]) {
-                "forward" -> Triple(
-                    t.first + n,
-                    t.second + t.third * n,
-                    t.third
-                )
-                "up" -> Triple(t.first, t.second, t.third - n)
-                "down" -> Triple(t.first, t.second, t.third + n)
-                else -> throw IllegalArgumentException("Unrecognized '$line' instruction")
-            }
-        }
-    return dest.first * dest.second
+data class Sub1(
+    override val pos: Int = 0,
+    override val depth: Int = 0
+) : Sub<Sub1> {
+
+    override fun forward(n: Int) =
+        copy(pos = pos + n)
+
+    override fun up(n: Int) =
+        copy(depth = depth - n)
+
+    override fun down(n: Int) =
+        copy(depth = depth + n)
+
 }
+
+private fun <T : Sub<T>> drive(sub: Sub<T>, line: String): Sub<T> {
+    val words = line.split(" ")
+    val n = words[1].toInt()
+    return when (words[0]) {
+        "forward" -> sub.forward(n)
+        "up" -> sub.up(n)
+        "down" -> sub.down(n)
+        else -> throw IllegalArgumentException("Unrecognized '$line' instruction")
+    }
+}
+
+fun partOne(input: String) =
+    input
+        .lineSequence()
+        .fold(Sub1(), ::drive)
+        .finalDestination
+
+data class Sub2(
+    override val pos: Int = 0,
+    override val depth: Int = 0,
+    val aim: Int = 0
+) : Sub<Sub2> {
+
+    override fun forward(n: Int) =
+        copy(pos = pos + n, depth = depth + aim * n)
+
+    override fun up(n: Int) =
+        copy(aim = aim - n)
+
+    override fun down(n: Int) =
+        copy(aim = aim + n)
+
+}
+
+fun partTwo(input: String) =
+    input
+        .lineSequence()
+        .fold(Sub2(), ::drive)
+        .finalDestination
