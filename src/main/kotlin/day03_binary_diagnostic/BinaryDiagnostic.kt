904f1a668627e6d761ef51edb61c4eb3f2d1020f
Barney Boisvert <bboisvert@armus.com>
2021-12-02 21:16:41 -0800

    part one

--- src/main/kotlin/day03_binary_diagnostic/BinaryDiagnostic.kt
+++ src/main/kotlin/day03_binary_diagnostic/BinaryDiagnostic.kt
@@ -1,10 +1,26 @@
 package day03_binary_diagnostic
 
 fun main() {
-    util.solve(::partOne)
+    util.solve(3687446, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+fun partOne(input: String): Long {
+    val freqs = IntArray(input.lineSequence().first().length)
+    for (line in input.lineSequence()) {
+        line.forEachIndexed { i, c ->
+            freqs[i] += if (c == '1') 1 else -1
+        }
+    }
+    var gamma = 0L
+    var epsilon = 0L
+    freqs.forEach { c ->
+        gamma = gamma shl 1
+        epsilon = epsilon shl 1
+        if (c > 0) ++gamma else ++epsilon
+    }
+    return gamma * epsilon
+}
+
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/day03_binary_diagnostic/BinaryDiagnosticKtTest.kt
+++ src/test/kotlin/day03_binary_diagnostic/BinaryDiagnosticKtTest.kt
@@ -4,14 +4,25 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 private val WORKED_EXAMPLE = """
-    input
+    00100
+    11110
+    10110
+    10111
+    10101
+    01111
+    00111
+    11100
+    10000
+    11001
+    00010
+    01010
 """.trimIndent()
 
 internal class BinaryDiagnosticKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+        assertEquals(198, partOne(WORKED_EXAMPLE))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
