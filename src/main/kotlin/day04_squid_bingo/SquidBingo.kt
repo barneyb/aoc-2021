2bf7b4bcf1af21f577138315ec32e5ffe64b672e
Barney Boisvert <bboisvert@armus.com>
2021-12-04 07:10:58 -0800

    skeleton

---
+++ src/main/kotlin/day04_squid_bingo/SquidBingo.kt
@@ -0,0 +1,10 @@
+package day04_squid_bingo
+
+fun main() {
+    util.solve(::partOne)
+    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
