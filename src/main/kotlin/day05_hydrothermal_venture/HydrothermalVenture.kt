2af8eb1cb8393b6507dba87f8e2b12d02271d4dc
Barney Boisvert <bboisvert@armus.com>
2021-12-05 06:43:06 -0800

    skeleton

---
+++ src/main/kotlin/day05_hydrothermal_venture/HydrothermalVenture.kt
@@ -0,0 +1,10 @@
+package day05_hydrothermal_venture
+
+fun main() {
+    util.solve(::partOne)
+    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
