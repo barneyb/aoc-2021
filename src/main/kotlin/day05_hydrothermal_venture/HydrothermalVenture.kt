51a4a6f25743821feced77fd35dc78bad6696311
Barney Boisvert <bboisvert@armus.com>
2021-12-05 07:16:11 -0800

    part one

--- src/main/kotlin/day05_hydrothermal_venture/HydrothermalVenture.kt
+++ src/main/kotlin/day05_hydrothermal_venture/HydrothermalVenture.kt
@@ -1,10 +1,71 @@
 package day05_hydrothermal_venture
 
+import geom2d.Point
+
 fun main() {
-    util.solve(::partOne)
+    util.solve(7644, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+data class Line(val start: Point, val end: Point) {
+
+    val horizontal
+        get() = start.y == end.y
+
+    val vertical
+        get() = start.x == end.x
+
+    val allPoints
+        get() = when {
+            horizontal -> (if (start.x < end.x) start.x..end.x else start.x downTo end.x)
+                .asSequence()
+                .map { start.copy(x = it) }
+            vertical -> (if (start.y < end.y) start.y..end.y else start.y downTo end.y)
+                .asSequence()
+                .map { start.copy(y = it) }
+            else -> throw UnsupportedOperationException("Only vertical and horizontal lines can have their points enumerated.")
+        }
+}
+
+private fun String.toLine() =
+    replace(" -> ", ",")
+        .split(',')
+        .map(String::toLong)
+        .let {
+            Line(
+                Point(it[0], it[1]),
+                Point(it[2], it[3]),
+            )
+        }
+
+fun partOne(input: String): Int {
+    val lines = input
+        .lines()
+        .map(String::toLine)
+        .filter { it.vertical || it.horizontal }
+    val hist = lines
+        .map(Line::allPoints)
+        .reduce(Sequence<Point>::plus)
+        .groupingBy { it }
+        .eachCount()
+//    val (mx, my) = lines.fold(Pair(0L, 0L)) { (mx, my), l ->
+//        Pair(
+//            max(mx, max(l.start.x, l.end.x)),
+//            max(my, max(l.start.y, l.end.y)),
+//        )
+//    }
+//    for (y in 0..mx) {
+//        for (x in 0..my) {
+//            print(
+//                when (val n = hist[(Point(x, y))]) {
+//                    null -> '.'
+//                    else -> n
+//                }
+//            )
+//        }
+//        println()
+//    }
+    return hist.count { it.value > 1 }
+}
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/day05_hydrothermal_venture/HydrothermalVentureKtTest.kt
+++ src/test/kotlin/day05_hydrothermal_venture/HydrothermalVentureKtTest.kt
@@ -4,14 +4,23 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 private val WORKED_EXAMPLE = """
-    input
+    0,9 -> 5,9
+    8,0 -> 0,8
+    9,4 -> 3,4
+    2,2 -> 2,1
+    7,0 -> 7,4
+    6,4 -> 2,0
+    0,9 -> 2,9
+    3,4 -> 1,4
+    0,0 -> 8,8
+    5,5 -> 8,2
 """.trimIndent()
 
 internal class HydrothermalVentureKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+        assertEquals(5, partOne(WORKED_EXAMPLE))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
