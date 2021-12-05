9c3027790018c7a75b949b3f1ce7802656c7298c
Barney Boisvert <bboisvert@armus.com>
2021-12-05 08:12:19 -0800

    holy cow are nested FlatteningSequence _slow_

--- src/main/kotlin/day05_hydrothermal_venture/HydrothermalVenture.kt
+++ src/main/kotlin/day05_hydrothermal_venture/HydrothermalVenture.kt
@@ -1,6 +1,7 @@
 package day05_hydrothermal_venture
 
 import geom2d.Point
+import histogram.count
 
 fun main() {
     util.solve(7644, ::partOne)
@@ -42,12 +43,14 @@ fun partOne(input: String): Int {
     val lines = input
         .lines()
         .map(String::toLine)
-        .filter { it.vertical || it.horizontal }
-    val hist = lines
-        .map(Line::allPoints)
-        .reduce(Sequence<Point>::plus)
-        .groupingBy { it }
-        .eachCount()
+    val hist = HashMap<Point, Int>()
+        .also { hist ->
+            lines
+                .asSequence()
+                .filter { it.vertical || it.horizontal }
+                .map(Line::allPoints)
+                .forEach { it.forEach(hist::count) }
+        }
 //    val (mx, my) = lines.fold(Pair(0L, 0L)) { (mx, my), l ->
 //        Pair(
 //            max(mx, max(l.start.x, l.end.x)),
