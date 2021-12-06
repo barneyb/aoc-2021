b9aed24b3ffc9bf005c37c7d379012677c420d15
Barney Boisvert <bboisvert@armus.com>
2021-12-06 06:13:34 -0800

    part one

--- src/main/kotlin/day06_lanternfish_growth/LanternfishGrowth.kt
+++ src/main/kotlin/day06_lanternfish_growth/LanternfishGrowth.kt
@@ -1,10 +1,30 @@
 package day06_lanternfish_growth
 
 fun main() {
-    util.solve(::partOne)
+    util.solve(371379, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+fun partOne(input: String) = partOne(input, 80)
+
+fun partOne(input: String, days: Int) =
+    (1..days)
+        .fold(
+            input.split(',')
+                .map(String::toInt)
+        ) { fish, _ ->
+            val next = mutableListOf<Int>()
+            fish.forEach {
+                when (it) {
+                    0 -> {
+                        next.add(6) // start
+                        next.add(8) // new fish
+                    }
+                    else -> next.add(it - 1) // decrement
+                }
+            }
+            next
+        }
+        .size
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/day06_lanternfish_growth/LanternfishGrowthKtTest.kt
+++ src/test/kotlin/day06_lanternfish_growth/LanternfishGrowthKtTest.kt
@@ -4,14 +4,34 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 private val WORKED_EXAMPLE = """
-    input
+    3,4,3,1,2
 """.trimIndent()
 
 internal class LanternfishGrowthKtTest {
 
     @Test
+    fun partOneDayZero() {
+        assertEquals(5, partOne(WORKED_EXAMPLE, 0))
+    }
+
+    @Test
+    fun partOneDayOne() {
+        assertEquals(5, partOne(WORKED_EXAMPLE, 1))
+    }
+
+    @Test
+    fun partOneDayTwo() {
+        assertEquals(6, partOne(WORKED_EXAMPLE, 2))
+    }
+
+    @Test
+    fun partOneDay18() {
+        assertEquals(26, partOne(WORKED_EXAMPLE, 18))
+    }
+
+    @Test
     fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+        assertEquals(5934, partOne(WORKED_EXAMPLE))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
