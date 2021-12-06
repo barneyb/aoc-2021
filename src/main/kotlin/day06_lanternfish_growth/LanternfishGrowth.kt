da32df4db1dbfb8a88124a91e9efdf47cb159c17
Barney Boisvert <bboisvert@armus.com>
2021-12-06 07:13:00 -0800

    pull some helpers out of simulate

--- src/main/kotlin/day06_lanternfish_growth/LanternfishGrowth.kt
+++ src/main/kotlin/day06_lanternfish_growth/LanternfishGrowth.kt
@@ -1,5 +1,6 @@
 package day06_lanternfish_growth
 
+import histogram.Histogram
 import histogram.buildHistogram
 import histogram.count
 import histogram.total
@@ -11,26 +12,33 @@ fun main() {
 
 fun partOne(input: String) = simulate(input, 80)
 
-fun simulate(input: String, days: Int): Long {
-    val initial = buildHistogram<Int>(9) {
-        input.split(',')
+private fun String.toCounts() =
+    buildHistogram<Int>(9) {
+        split(',')
             .map(String::toInt)
             .forEach { count(it, 1) }
     }
-    return (1..days)
-        .fold(initial) { fish, _ ->
-            buildHistogram(9) {
-                fish.entries.forEach { (timer, count) ->
-                    if (timer == 0) {
-                        count(6, count) // reset
-                        count(8, count) // birth
-                    } else {
-                        count(timer - 1, count) // decrement
-                    }
-                }
+
+private fun nextGeneration(curr: Histogram<Int>) =
+    buildHistogram<Int>(9) {
+        curr.entries.forEach { (timer, count) ->
+            if (timer == 0) {
+                count(6, count) // reset
+                count(8, count) // birth
+            } else {
+                count(timer - 1, count) // decrement
             }
         }
+    }
+
+fun simulate(input: String, days: Int) =
+    simulate(input.toCounts(), days)
+
+fun simulate(initial: Histogram<Int>, days: Int) =
+    (1..days)
+        .fold(initial) { counts, _ ->
+            nextGeneration(counts)
+        }
         .total
-}
 
 fun partTwo(input: String) = simulate(input, 256)
