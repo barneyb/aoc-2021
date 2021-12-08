0a55fe66d2470c12f4866488cee01fc58e604641
Barney Boisvert <bboisvert@armus.com>
2021-12-08 14:16:28 -0800

    part one

--- src/main/kotlin/day08_seven_segment/SevenSegment.kt
+++ src/main/kotlin/day08_seven_segment/SevenSegment.kt
@@ -1,10 +1,16 @@
 package day08_seven_segment
 
 fun main() {
-    util.solve(::partOne)
+    util.solve(390, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+fun partOne(input: String) =
+    input
+        .lineSequence()
+        .map { it.substring(61) }
+        .flatMap { it.split(' ') }
+        .map(String::length)
+        .count { it == 2 || it == 3 || it == 4 || it == 7 }
 
 fun partTwo(input: String) = input.trim().length
