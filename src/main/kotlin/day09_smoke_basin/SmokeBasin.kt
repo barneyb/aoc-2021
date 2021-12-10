e801623004ea2a1330cfdd5c29ed6e059e2895a1
Barney Boisvert <bboisvert@armus.com>
2021-12-09 21:10:44 -0800

    part one

--- src/main/kotlin/day09_smoke_basin/SmokeBasin.kt
+++ src/main/kotlin/day09_smoke_basin/SmokeBasin.kt
@@ -1,10 +1,31 @@
 package day09_smoke_basin
 
+import geom2d.Point
+
 fun main() {
-    util.solve(::partOne)
+    util.solve(554, ::partOne) // 1774 is too high
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+fun partOne(input: String): Int {
+    val width = input.indexOf('\n')
+    val grid = input
+        .filter { it != '\n' }
+    val height = grid.length / width
+    val lowPoints = mutableListOf<Point>()
+    for (r in 0 until height) {
+        for (c in 0 until width) {
+            val n = grid[r * width + c]
+            if (r > 0 && n >= grid[(r - 1) * width + c]) continue
+            if (r < height - 1 && n >= grid[(r + 1) * width + c]) continue
+            if (c > 0 && n >= grid[r * width + c - 1]) continue
+            if (c < width - 1 && n >= grid[r * width + c + 1]) continue
+            lowPoints.add(Point(c.toLong(), r.toLong()))
+        }
+    }
+    return lowPoints
+        .map { p -> grid[(p.y * width + p.x).toInt()] }
+        .sumOf { it.digitToInt() + 1 }
+}
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/day09_smoke_basin/SmokeBasinKtTest.kt
+++ src/test/kotlin/day09_smoke_basin/SmokeBasinKtTest.kt
@@ -4,14 +4,108 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 private val WORKED_EXAMPLE = """
-    input
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
 """.trimIndent()
 
 internal class SmokeBasinKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+        assertEquals(
+            1, partOne(
+                """
+            011
+            111
+            111
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            1, partOne(
+                """
+            110
+            111
+            111
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            1, partOne(
+                """
+            111
+            111
+            110
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            1, partOne(
+                """
+            111
+            111
+            011
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            4, partOne(
+                """
+            010
+            111
+            010
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            1, partOne(
+                """
+            101
+            111
+            111
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            1, partOne(
+                """
+            111
+            110
+            111
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            1, partOne(
+                """
+            111
+            111
+            101
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            1, partOne(
+                """
+            111
+            011
+            111
+        """.trimIndent()
+            )
+        )
+        assertEquals(
+            4, partOne(
+                """
+            101
+            010
+            101
+        """.trimIndent()
+            )
+        )
+        assertEquals(15, partOne(WORKED_EXAMPLE))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
