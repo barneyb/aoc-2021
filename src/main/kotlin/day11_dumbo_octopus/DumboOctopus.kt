9e31321ade096b56a3ba0f1495e8f3ba8d069785
Barney Boisvert <bboisvert@armus.com>
2021-12-11 08:11:48 -0800

    skeleton

---
+++ src/main/kotlin/day11_dumbo_octopus/DumboOctopus.kt
@@ -0,0 +1,10 @@
+package day11_dumbo_octopus
+
+fun main() {
+    util.solve(::partOne)
+    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
---
+++ src/test/kotlin/day11_dumbo_octopus/DumboOctopusKtTest.kt
@@ -0,0 +1,23 @@
+package day11_dumbo_octopus
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+private val WORKED_EXAMPLE = """
+    input
+""".trimIndent()
+
+internal class DumboOctopusKtTest {
+
+    @Test
+    fun partOne() {
+        assertEquals(0, partOne(WORKED_EXAMPLE))
+    }
+
+    @kotlin.test.Ignore // todo: reinstate when ready!
+    @Test
+    fun partTwo() {
+        assertEquals(0, partTwo(WORKED_EXAMPLE))
+    }
+
+}
