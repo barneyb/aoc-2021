90d14ef25408847e07e0541cd1beb7d3fc978b9f
Barney Boisvert <bboisvert@armus.com>
2021-12-12 10:02:49 -0800

    part one

--- src/main/kotlin/day12_passage_pathing/PassagePathing.kt
+++ src/main/kotlin/day12_passage_pathing/PassagePathing.kt
@@ -1,10 +1,51 @@
 package day12_passage_pathing
 
+import java.util.*
+
 fun main() {
-    util.solve(::partOne)
+    util.solve(5333, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+fun partOne(input: String): Int {
+    // this is sorta gross. encode the strings!
+    val graph: Map<String, List<String>> =
+        mutableMapOf<String, MutableList<String>>()
+            .apply {
+                input
+                    .lines()
+                    .map { it.split('-') }
+                    .map { (a, b) ->
+                        getOrPut(a, ::mutableListOf).add(b)
+                        getOrPut(b, ::mutableListOf).add(a)
+                    }
+            }
+
+    data class Path(val at: String, val visited: Set<String>) {
+
+        constructor(at: String) : this(at, setOf(at))
+
+        fun then(cave: String) =
+            Path(cave, visited + cave)
+
+    }
+
+    var pathCount = 0
+    val queue: Queue<Path> = LinkedList()
+    queue.add(Path("start"))
+    while (queue.isNotEmpty()) {
+        val p = queue.remove()
+        if (p.at == "end") {
+            pathCount += 1
+            continue
+        }
+        queue.addAll(
+            graph[p.at]!!
+                .filter { it[0].isUpperCase() || !p.visited.contains(it) }
+                .map { p.then(it) }
+        )
+    }
+    return pathCount
+}
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/day12_passage_pathing/PassagePathingKtTest.kt
+++ src/test/kotlin/day12_passage_pathing/PassagePathingKtTest.kt
@@ -3,21 +3,71 @@ package day12_passage_pathing
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-private val WORKED_EXAMPLE = """
-    input
+private val EXAMPLE_SMALL = """
+    start-A
+    start-b
+    A-c
+    A-b
+    b-d
+    A-end
+    b-end
+""".trimIndent()
+
+private val EXAMPLE_MEDIUM = """
+    dc-end
+    HN-start
+    start-kj
+    dc-start
+    dc-HN
+    LN-dc
+    HN-end
+    kj-sa
+    kj-HN
+    kj-dc
+""".trimIndent()
+
+private val EXAMPLE_LARGE = """
+    fs-end
+    he-DX
+    fs-he
+    start-DX
+    pj-DX
+    end-zg
+    zg-sl
+    zg-pj
+    pj-he
+    RW-he
+    fs-DX
+    pj-RW
+    zg-RW
+    start-pj
+    he-WI
+    zg-he
+    pj-fs
+    start-RW
 """.trimIndent()
 
 internal class PassagePathingKtTest {
 
     @Test
-    fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+    fun partOne_small() {
+        assertEquals(10, partOne(EXAMPLE_SMALL))
+    }
+
+    @Test
+    fun partOne_medium() {
+        assertEquals(19, partOne(EXAMPLE_MEDIUM))
+    }
+
+    @Test
+    fun partOne_large() {
+        assertEquals(226, partOne(EXAMPLE_LARGE))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
     @Test
     fun partTwo() {
-        assertEquals(0, partTwo(WORKED_EXAMPLE))
+        assertEquals(0, partTwo(EXAMPLE_SMALL))
     }
 
 }
