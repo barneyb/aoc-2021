dc16e3a6f465b7f6c0f5852de6518b6b344c47bf
Barney Boisvert <bboisvert@armus.com>
2021-12-11 20:40:46 -0800

    Revert "pull CharGrid out of smoke basins"
    This reverts commit c1e35a41cf529eb6a0b2ab684c2fb969e1233052.

--- src/main/kotlin/day09_smoke_basin/SmokeBasin.kt
+++ src/main/kotlin/day09_smoke_basin/SmokeBasin.kt
@@ -1,30 +1,38 @@
 package day09_smoke_basin
 
-import geom2d.CharGrid
+import geom2d.Dir
 import geom2d.Point
 import util.saveTextFile
 import java.io.PrintWriter
 import java.util.*
 
 fun main() {
-    util.solve(554, ::partOne) // 1774 is too high (<= not <, idiot)
-    // 115.889μs initially
-    // 563.956μs w/ extracted Grid type
+    util.solve(554, ::partOne) // 1774 is too high
     util.solve(1017792, ::partTwo)
-    // 943.85μs initially
-    // 1.410678ms w/ extracted Grid type
     saveTextFile(::csv, "csv")
 }
 
-private class Grid(input: String) : CharGrid<Int>(input, Char::digitToInt) {
+private class Grid(input: String) {
+    val width: Int
+    val grid: String
+    val height: Int
     val lowPoints: Set<Point>
 
     init {
+        width = input.indexOf('\n')
+        grid = input
+            .filter { it != '\n' }
+        height = grid.length / width
         lowPoints = mutableSetOf()
-        allPoints().forEach { p ->
-            val n = get(p)
-            if (neighbors(p).all { get(it) > n })
-                lowPoints.add(p)
+        for (r in 0 until height) {
+            for (c in 0 until width) {
+                val n = grid[r * width + c]
+                if (r > 0 && n >= grid[(r - 1) * width + c]) continue
+                if (r < height - 1 && n >= grid[(r + 1) * width + c]) continue
+                if (c > 0 && n >= grid[r * width + c - 1]) continue
+                if (c < width - 1 && n >= grid[r * width + c + 1]) continue
+                lowPoints.add(Point(c.toLong(), r.toLong()))
+            }
         }
     }
 
@@ -38,11 +46,17 @@ private class Grid(input: String) : CharGrid<Int>(input, Char::digitToInt) {
                 if (basin.contains(p)) continue
                 if (get(p) == 9) continue
                 basin.add(p)
-                queue.addAll(neighbors(p))
+                if (p.y > 0) queue.add(p.step(Dir.NORTH))
+                if (p.y < height - 1) queue.add(p.step(Dir.SOUTH))
+                if (p.x > 0) queue.add(p.step(Dir.WEST))
+                if (p.x < width - 1) queue.add(p.step(Dir.EAST))
             }
             basin
         }
     }
+
+    operator fun get(p: Point) =
+        grid[(p.y * width + p.x).toInt()].digitToInt()
 }
 
 fun partOne(input: String) =
@@ -51,16 +65,12 @@ fun partOne(input: String) =
     }
 
 fun partTwo(input: String) =
-    TreeSet<Int>() // poor-man's "heap"
-        .also { pq ->
-            Grid(input)
-                .basins
-                .map { it.size }
-                .forEach {
-                    pq.add(it)
-                    if (pq.size > 3) pq.remove(pq.first())
-                }
-        }
+    Grid(input)
+        .basins
+        .map { it.size }
+        .sorted()       // should use a priority queue
+        .asReversed()   // here, but I don't have one
+        .take(3)        // readily available...
         .reduce(Int::times)
 
 private fun csv(input: String, out: PrintWriter) {
