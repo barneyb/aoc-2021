04bba69e7decc1d7e841a40e3adaffdd5ab7ea35
Barney Boisvert <bboisvert@armus.com>
2021-12-13 19:07:46 -0800

    part one

--- src/main/kotlin/day13_transparent_paper/TransparentPaper.kt
+++ src/main/kotlin/day13_transparent_paper/TransparentPaper.kt
@@ -1,10 +1,48 @@
 package day13_transparent_paper
 
+import geom2d.Point
+
 fun main() {
-    util.solve(::partOne)
+    util.solve(765, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+fun partOne(input: String): Int {
+    val (dotSpecs, foldSpecs) = input
+        .split("\n\n")
+    val dots = dotSpecs
+        .lineSequence()
+        .map {
+            it.split(',')
+                .map(String::toLong)
+        }
+        .map { Point(it.first(), it.last()) }
+        .toSet()
+    val folds = foldSpecs
+        .lines()
+        .map {
+            it
+                .split(' ')[2]
+                .split('=')
+        }
+        .map { Pair(it.first()[0], it.last().toInt()) }
+    return folds
+        .take(1)
+        .fold(dots) { ds, (a, n) ->
+            when (a) {
+                'x' -> ds.map { p ->
+                    if (p.x <= n) p
+                    else p.copy(x = n - (p.x - n))
+                }
+                'y' -> ds.map { p ->
+                    if (p.y > n) p
+                    else p.copy(y = n - (p.y - n))
+                }
+                else -> throw IllegalArgumentException("Unknown fold along '$a'?!")
+            }
+                .toSet()
+        }
+        .size
+}
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/day13_transparent_paper/TransparentPaperKtTest.kt
+++ src/test/kotlin/day13_transparent_paper/TransparentPaperKtTest.kt
@@ -4,14 +4,34 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 private val WORKED_EXAMPLE = """
-    input
+    6,10
+    0,14
+    9,10
+    0,3
+    10,4
+    4,11
+    6,0
+    6,12
+    4,1
+    0,13
+    10,12
+    3,4
+    3,0
+    8,4
+    1,10
+    2,14
+    8,10
+    9,0
+
+    fold along y=7
+    fold along x=5
 """.trimIndent()
 
 internal class TransparentPaperKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+        assertEquals(17, partOne(WORKED_EXAMPLE))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
