c8fe325f6d1318c6aeda4f1ce495fc0fbc42746f
Barney Boisvert <bboisvert@armus.com>
2021-12-14 16:20:25 -0800

    skeleton

---
+++ src/main/kotlin/day14_extended_polymerization/ExtendedPolymerization.kt
@@ -0,0 +1,10 @@
+package day14_extended_polymerization
+
+fun main() {
+    util.solve(::partOne)
+    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
---
+++ src/test/kotlin/day14_extended_polymerization/ExtendedPolymerizationKtTest.kt
@@ -0,0 +1,23 @@
+package day14_extended_polymerization
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+private val WORKED_EXAMPLE = """
+    input
+""".trimIndent()
+
+internal class ExtendedPolymerizationKtTest {
+
+    @Test
+    fun partOne() {
+        assertEquals(0, partOne(WORKED_EXAMPLE))
+    }
+
+    @kotlin.test.Ignore // todo: reinstate when ready!
+    @Test
+    fun partTwo() {
+        assertEquals(0, partTwo(WORKED_EXAMPLE))
+    }
+
+}
