6f380d1d4b92264be6708964d0eec008b09dd38c
Barney Boisvert <bboisvert@armus.com>
2021-12-14 17:19:15 -0800

    part two

--- src/main/kotlin/day14_extended_polymerization/ExtendedPolymerization.kt
+++ src/main/kotlin/day14_extended_polymerization/ExtendedPolymerization.kt
@@ -1,28 +1,50 @@
 package day14_extended_polymerization
 
+import histogram.count
 import histogram.histogramOf
+import histogram.mutableHistogramOf
 
 fun main() {
     util.solve(2891, ::partOne)
-    util.solve(::partTwo)
+    // 975.249μs initially
+    // 188.474μs w/ part two
+    util.solve(4607749009683, ::partTwo)
 }
 
-fun partOne(input: String): Long {
+fun partOne(input: String) =
+    solve(input, 10)
+
+internal fun solve(input: String, steps: Int): Long {
     val lines = input.lines()
     val template = lines.first()
     val rules = lines.drop(2)
         .map { it.split(" -> ") }
-        .associate { it.first().let { Pair(it[0], it[1]) } to it.last() }
-    val result = (1..10).fold(template) { tmpl, _ ->
-        tmpl[0] + tmpl
-            .zipWithNext()
-            .joinToString(separator = "") {
-                val c = rules[it]!!
-                "$c${it.second}"
+        .associate { rule ->
+            rule.first()
+                .let { it to listOf(it[0] + rule.last(), rule.last() + it[1]) }
+        }
+    val initial = histogramOf(template
+        .zipWithNext { a, b -> "$a$b" })
+    val hist = (1..steps).fold(initial) { hist, step ->
+        val next = mutableHistogramOf<String>()
+        for ((k, v) in hist) {
+            rules[k]!!.forEach {
+                next.count(it, v)
             }
+        }
+        next
     }
-    val hist = histogramOf(result.asSequence())
-    return hist.values.maxOrNull()!! - hist.values.minOrNull()!!
+    val chars = mutableHistogramOf<Char>()
+    chars.count(template.first())
+    chars.count(template.last())
+    for ((p, n) in hist) {
+        chars.count(p[0], n)
+        chars.count(p[1], n)
+    }
+    val diff = chars.values.maxOrNull()!! - chars.values.minOrNull()!!
+    if (diff % 2L == 1L) throw IllegalStateException("got an odd number?")
+    return diff / 2
 }
 
-fun partTwo(input: String) = input.trim().length
+fun partTwo(input: String) =
+    solve(input, 40)
--- src/test/kotlin/day14_extended_polymerization/ExtendedPolymerizationKtTest.kt
+++ src/test/kotlin/day14_extended_polymerization/ExtendedPolymerizationKtTest.kt
@@ -27,14 +27,33 @@ private val WORKED_EXAMPLE = """
 internal class ExtendedPolymerizationKtTest {
 
     @Test
+    fun solve0() {
+        assertEquals(1, solve(WORKED_EXAMPLE, 0))
+    }
+
+    @Test
+    fun solve1() {
+        assertEquals(1, solve(WORKED_EXAMPLE, 1))
+    }
+
+    @Test
+    fun solve2() {
+        assertEquals(5, solve(WORKED_EXAMPLE, 2))
+    }
+
+    @Test
+    fun solve3() {
+        assertEquals(7, solve(WORKED_EXAMPLE, 3))
+    }
+
+    @Test
     fun partOne() {
         assertEquals(1588, partOne(WORKED_EXAMPLE))
     }
 
-    @kotlin.test.Ignore // todo: reinstate when ready!
     @Test
     fun partTwo() {
-        assertEquals(0, partTwo(WORKED_EXAMPLE))
+        assertEquals(2188189693529, partTwo(WORKED_EXAMPLE))
     }
 
 }
