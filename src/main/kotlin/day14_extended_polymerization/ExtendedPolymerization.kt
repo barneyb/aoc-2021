d743ca1a20260a91a3c2719c84b588dac1c29b13
Barney Boisvert <bboisvert@armus.com>
2021-12-14 16:40:50 -0800

    part one

--- src/main/kotlin/day14_extended_polymerization/ExtendedPolymerization.kt
+++ src/main/kotlin/day14_extended_polymerization/ExtendedPolymerization.kt
@@ -1,10 +1,28 @@
 package day14_extended_polymerization
 
+import histogram.histogramOf
+
 fun main() {
-    util.solve(::partOne)
+    util.solve(2891, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+fun partOne(input: String): Long {
+    val lines = input.lines()
+    val template = lines.first()
+    val rules = lines.drop(2)
+        .map { it.split(" -> ") }
+        .associate { it.first().let { Pair(it[0], it[1]) } to it.last() }
+    val result = (1..10).fold(template) { tmpl, _ ->
+        tmpl[0] + tmpl
+            .zipWithNext()
+            .joinToString(separator = "") {
+                val c = rules[it]!!
+                "$c${it.second}"
+            }
+    }
+    val hist = histogramOf(result.asSequence())
+    return hist.values.maxOrNull()!! - hist.values.minOrNull()!!
+}
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/day14_extended_polymerization/ExtendedPolymerizationKtTest.kt
+++ src/test/kotlin/day14_extended_polymerization/ExtendedPolymerizationKtTest.kt
@@ -4,14 +4,31 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 private val WORKED_EXAMPLE = """
-    input
+    NNCB
+
+    CH -> B
+    HH -> N
+    CB -> H
+    NH -> C
+    HB -> C
+    HC -> B
+    HN -> C
+    NN -> C
+    BH -> H
+    NC -> B
+    NB -> B
+    BN -> B
+    BB -> N
+    BC -> B
+    CC -> N
+    CN -> C
 """.trimIndent()
 
 internal class ExtendedPolymerizationKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+        assertEquals(1588, partOne(WORKED_EXAMPLE))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
