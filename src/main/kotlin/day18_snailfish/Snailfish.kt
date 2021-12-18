3e9106ee53192ad4d247ee41392e322f63e92bfd
Barney Boisvert <bboisvert@armus.com>
2021-12-18 14:46:07 -0800

    skeleton

---
+++ src/main/kotlin/day18_snailfish/Snailfish.kt
@@ -0,0 +1,10 @@
+package day18_snailfish
+
+fun main() {
+    util.solve(::partOne)
+    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
