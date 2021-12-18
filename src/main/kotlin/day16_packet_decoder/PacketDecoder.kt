f1dc137c07278bf0b7e82cf8f974880358dc71a8
Barney Boisvert <bboisvert@armus.com>
2021-12-17 16:30:59 -0800

    part one

--- src/main/kotlin/day16_packet_decoder/PacketDecoder.kt
+++ src/main/kotlin/day16_packet_decoder/PacketDecoder.kt
@@ -1,10 +1,114 @@
 package day16_packet_decoder
 
 fun main() {
-    util.solve(::partOne)
+    util.solve(960, ::partOne)
     util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+interface Packet {
+    val version: Int
+    val type: Int
+    val versionSum: Int
+}
+
+data class Literal(
+    override val version: Int,
+    override val type: Int,
+    val value: Int
+) : Packet {
+    override val versionSum: Int
+        get() = version
+}
+
+data class Operator(
+    override val version: Int,
+    override val type: Int,
+    val operands: List<Packet>
+) : Packet {
+    override val versionSum: Int
+        get() =
+            version + operands.sumOf(Packet::versionSum)
+}
+
+class CountingIterator<T>(val delegate: Iterator<T>) : Iterator<T> {
+    var count: Int = 0
+
+    override fun hasNext() =
+        delegate.hasNext()
+
+    override fun next(): T {
+        count += 1
+        return delegate.next()
+    }
+}
+
+fun <T> Iterator<T>.countingIterator() =
+    CountingIterator(this)
+
+fun <T> Iterable<T>.countingIterator() =
+    CountingIterator(iterator())
+
+fun <T> Sequence<T>.countingIterator() =
+    CountingIterator(iterator())
+
+fun String.asHexBitSequence(): CountingIterator<Boolean> {
+    val hexDigits = iterator()
+    var buffer = 0
+    var bitIdx = -1
+    // sorta silly...
+    return generateSequence {
+        if (bitIdx < 0) {
+            if (!hexDigits.hasNext()) return@generateSequence null
+            bitIdx = 3
+            buffer = hexDigits.next().digitToInt(16)
+        }
+        buffer and (1 shl bitIdx--) != 0
+    }.countingIterator()
+}
+
+fun Iterator<Boolean>.readInt(bits: Int): Int =
+    (0 until bits).fold(0) { incoming, _ ->
+        (incoming shl 1) + if (next()) 1 else 0
+    }
+
+fun CountingIterator<Boolean>.readPacket(): Packet {
+    val version = readInt(3)
+    val typeId = readInt(3)
+    if (typeId == 4) {
+        return Literal(version, typeId, readLiteral())
+    } else if (next()) {
+        // packet-length operator
+        val len = readInt(11)
+        return Operator(
+            version,
+            typeId,
+            (0 until len).map { readPacket() },
+        )
+    } else {
+        // bit-length operator
+        val operands = mutableListOf<Packet>()
+        val len = readInt(15)
+        val end = count + len
+        while (count < end) operands.add(readPacket())
+        return Operator(version, typeId, operands)
+    }
+}
+
+private fun Iterator<Boolean>.readLiteral(): Int {
+    var v = 0
+    do {
+        val segment = readInt(5)
+        v = (v shl 4) + (segment and 0b01111)
+    } while (segment and 0b10000 > 1)
+    return v
+}
+
+fun String.toPacket(): Packet =
+    asHexBitSequence().readPacket()
+
+fun partOne(input: String): Int {
+    val packet = input.toPacket()
+    return packet.versionSum
+}
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/day16_packet_decoder/PacketDecoderKtTest.kt
+++ src/test/kotlin/day16_packet_decoder/PacketDecoderKtTest.kt
@@ -1,23 +1,71 @@
 package day16_packet_decoder
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
-
-private val WORKED_EXAMPLE = """
-    input
-""".trimIndent()
+import kotlin.test.assertTrue
 
 internal class PacketDecoderKtTest {
 
     @Test
-    fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+    fun bitSeq() {
+        val itr = "D2".asHexBitSequence()
+        assertTrue(itr.next())
+        assertTrue(itr.next())
+        assertFalse(itr.next())
+        assertTrue(itr.next())
+
+        assertFalse(itr.next())
+        assertFalse(itr.next())
+        assertTrue(itr.next())
+        assertFalse(itr.next())
+
+        assertFalse(itr.hasNext())
     }
 
-    @kotlin.test.Ignore // todo: reinstate when ready!
     @Test
-    fun partTwo() {
-        assertEquals(0, partTwo(WORKED_EXAMPLE))
+    fun readInt() {
+        val itr = "D2".asHexBitSequence()
+        assertEquals(6, itr.readInt(3))
+        assertEquals(4, itr.readInt(3))
+    }
+
+    @Test
+    fun parseLiteral() {
+        assertEquals(Literal(6, 4, 2021), "D2FE28".toPacket())
+    }
+
+    @Test
+    fun parseBitLenOp() {
+        assertEquals(
+            Operator(
+                1, 6, listOf(
+                    Literal(6, 4, 10),
+                    Literal(2, 4, 20),
+                )
+            ), "38006F45291200".toPacket()
+        )
+    }
+
+    @Test
+    fun parsePacketLenOp() {
+        assertEquals(
+            Operator(
+                7, 3, listOf(
+                    Literal(2, 4, 1),
+                    Literal(4, 4, 2),
+                    Literal(1, 4, 3),
+                )
+            ), "EE00D40C823060".toPacket()
+        )
+    }
+
+    @Test
+    fun partOne() {
+        assertEquals(16, partOne("8A004A801A8002F478"))
+        assertEquals(12, partOne("620080001611562C8802118E34"))
+        assertEquals(23, partOne("C0015000016115A2E0802F182340"))
+        assertEquals(31, partOne("A0016C880162017C3686B18A3D4780"))
     }
 
 }
