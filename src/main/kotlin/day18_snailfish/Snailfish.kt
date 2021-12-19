735db56f7c26625424615e63e9a96422f7d7f617
Barney Boisvert <bboisvert@armus.com>
2021-12-19 12:34:09 -0800

    switch to tokens

--- src/main/kotlin/day18_snailfish/Snailfish.kt
+++ src/main/kotlin/day18_snailfish/Snailfish.kt
@@ -1,5 +1,7 @@
 package day18_snailfish
 
+import util.countingIterator
+
 /**
  * todo: add notes
  */
@@ -8,104 +10,158 @@ fun main() {
     util.solve(::partTwo)
 }
 
-private data class IntAt(val value: Int, val start: Int, val end: Int) {
-    val length get() = end - start
+private const val T_OPEN = -100
+private const val T_DELIM = -1
+private const val T_CLOSE = -101
+
+data class SnailNum(var tokens: MutableList<Int>) {
+    var mutationCount: Int = 0
+    val size get() = tokens.size
+
+    fun get(index: Int) = tokens[index]
+
+    fun explode() {
+        var depth = 0
+        var idxToExplode = -1
+        for (i in tokens.indices) {
+            val c = tokens[i]
+            if (c == T_OPEN) depth++
+            else if (c == T_CLOSE) --depth
+            else if (depth > 4) {
+                idxToExplode = i
+                break
+            }
+        }
+        if (idxToExplode < 0) return // nothing to do
+
+        val left = tokens[idxToExplode]
+        val right = tokens[idxToExplode + 2]
+        // do these right-to-left so indexes don't change
+        for (i in idxToExplode + 3 until size) {
+            if (tokens[i] >= 0) {
+                tokens[i] += right
+                mutationCount += 1
+                break
+            }
+        }
+        replace(idxToExplode - 1, idxToExplode + 4, listOf(0))
+        for (i in idxToExplode - 2 downTo 0) {
+            if (tokens[i] >= 0) {
+                tokens[i] += left
+                mutationCount += 1
+                break
+            }
+        }
+    }
+
+    private fun replace(start: Int, end: Int, stuff: List<Int>) {
+        val next = ArrayList<Int>(size - (end - start) + stuff.size)
+        next.addAll(tokens.subList(0, start))
+        next.addAll(stuff)
+        if (end < size - 1)
+            next.addAll(tokens.subList(end, size))
+        tokens = next
+        mutationCount += 1
+    }
+
+    fun split() {
+        for (i in 0 until size) {
+            if (tokens[i] >= 10) {
+                val a = tokens[i] / 2
+                val b = tokens[i] - a
+                replace(i, i + 1, listOf(T_OPEN, a, T_DELIM, b, T_CLOSE))
+                break
+            }
+        }
+    }
+
+    fun reduce() {
+        while (true) {
+            val mc = mutationCount
+            explode()
+            if (mc < mutationCount) {
+                continue
+            }
+            split()
+            if (mc < mutationCount) {
+                continue
+            }
+            break
+        }
+    }
+
+    operator fun plus(other: SnailNum): SnailNum {
+        val tokens = ArrayList<Int>(3 + size + other.size)
+        tokens.add(T_OPEN)
+        tokens.addAll(this.tokens)
+        tokens.add(T_DELIM)
+        tokens.addAll(other.tokens)
+        tokens.add(T_CLOSE)
+        val num = SnailNum(tokens)
+        num.reduce()
+        return num
+    }
+
+    override fun toString(): String {
+        val sb = StringBuilder()
+        for (t in tokens) {
+            when {
+                t == T_OPEN -> sb.append('[')
+                t == T_DELIM -> sb.append(',')
+                t == T_CLOSE -> sb.append(']')
+                else -> sb.append(t)
+            }
+        }
+        return sb.toString()
+    }
+
 }
 
-private fun CharSequence.intAt(i: Int): IntAt {
-    var end = i + 1
-    while (end < length && this[end].isDigit()) end++
-    return IntAt(substring(i, end).toInt(), i, end)
+
+fun String.toSnailNum(): SnailNum {
+    val tokens = mutableListOf<Int>()
+    val itr = iterator().countingIterator()
+    for (c in itr) {
+        when (c) {
+            in '0'..'9' -> {
+                val n = c.digitToInt()
+                if (tokens.isEmpty() || tokens.last() < 0)
+                    tokens.add(n)
+                else
+                    tokens[tokens.size - 1] = tokens.last() * 10 + n
+            }
+            '[' -> tokens.add(T_OPEN)
+            ',' -> tokens.add(T_DELIM)
+            ']' -> tokens.add(T_CLOSE)
+            ' ', '\t', '\n' -> {}
+            else -> throw IllegalArgumentException("Unknown '$c' at ${itr.count}")
+        }
+    }
+    return SnailNum(tokens)
 }
 
 fun String.snailExplode(): String {
-    var depth = 0
-    var idxToExplode = -1
-    for (i in this.indices) {
-        val c = this[i]
-        if (c == '[') depth++
-        else if (c == ']') --depth
-        else if (depth > 4) {
-            idxToExplode = i
-            break
-        }
-    }
-    if (idxToExplode < 0) return this
-    val left = intAt(idxToExplode)
-    val right = intAt(left.end + 1/* comma */)
-    val sb = StringBuilder(this)
-    fun addAtIndex(idx: Int, other: IntAt) {
-        val it = sb.intAt(idx)
-        sb.replace(
-            it.start,
-            it.end,
-            (it.value + other.value).toString()
-        )
-    }
-    // do these right-to-left so indexes don't change
-    for (i in right.end + 1 until length) {
-        if (this[i] in '0'..'9') {
-            addAtIndex(i, right)
-            break
-        }
-    }
-    sb.replace(
-        left.start - 1/* open brace */,
-        right.end + 1 /* close brace */,
-        "0"
-    )
-    for (i in idxToExplode - 1 downTo 0) {
-        if (this[i] in '0'..'9') {
-            // and seek back to start of number...
-            var j = i - 1
-            while (this[j] in '0'..'9') j--
-            addAtIndex(j + 1, left)
-            break
-        }
-    }
-    return sb.toString()
+    val num = toSnailNum()
+    num.explode()
+    return num.toString()
 }
 
 fun String.snailSplit(): String {
-    for (i in 0 until length - 1) {
-        if (this[i] in '0'..'9' && this[i + 1] in '0'..'9') {
-            val it = intAt(i)
-            val a = it.value / 2
-            val b = it.value - a
-            return replaceRange(it.start, it.end, "[$a,$b]")
-        }
-    }
-    return this
+    val num = toSnailNum()
+    num.split()
+    return num.toString()
 }
 
 fun String.snailReduce(): String {
-    var curr = this
-//    println("reducing: $this")
-    while (true) {
-        var next = curr.snailExplode()
-        if (next != curr) {
-//            println(" explode: $next")
-            curr = next
-            continue
-        }
-        next = curr.snailSplit()
-        if (next != curr) {
-//            println("   split: $next")
-            curr = next
-            continue
-        }
-//        println("reduced : $next")
-        return next // done!
-    }
+    val num = toSnailNum()
+    num.reduce()
+    return num.toString()
 }
 
-fun String.snailPlus(other: String): String {
-    val result = "[$this,$other]".snailReduce()
-//    println("\n  $this\n+ $other\n= $result\n")
-    return result
-}
+fun String.snailPlus(other: String) =
+    toSnailNum().plus(other.toSnailNum()).toString()
 
-val String.snailMagnitude: Int
+val SnailNum.snailMagnitude: Int
     get() {
         TODO("Not yet implemented")
     }
