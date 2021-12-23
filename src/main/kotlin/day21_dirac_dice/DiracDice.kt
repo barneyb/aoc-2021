d9fd68acf3fc50facf93d2460430550340506a38
Barney Boisvert <bboisvert@armus.com>
2021-12-22 16:59:54 -0800

    part one

--- src/main/kotlin/day21_dirac_dice/DiracDice.kt
+++ src/main/kotlin/day21_dirac_dice/DiracDice.kt
@@ -4,10 +4,46 @@ package day21_dirac_dice
  * todo: add notes
  */
 fun main() {
-    util.solve(::partOne)
+    util.solve(504972, ::partOne)
 //    util.solve(::partTwo)
 }
 
-fun partOne(input: String) = input.length
+data class Player(var pos: Int, var score: Int) {
+    fun move(n: Int) {
+        pos = (pos - 1 + n) % 10 + 1
+        score += pos
+    }
+}
+
+data class Die(val sides: Int = 100) {
+    var rollCount: Int = 0
+    val value get() = (rollCount - 1) % sides + 1
+
+    fun roll(): Int {
+        rollCount += 1
+        return value
+    }
+}
+
+fun partOne(input: String): Int {
+    val (a, b) = input.lines()
+        .map { it.split(' ')[4].toInt() }
+        .map { Player(it, 0) }
+    val die = Die()
+    val loser: Player
+    while (true) {
+        a.move(die.roll() + die.roll() + die.roll())
+        if (a.score >= 1000) {
+            loser = b
+            break
+        }
+        b.move(die.roll() + die.roll() + die.roll())
+        if (b.score >= 1000) {
+            loser = a
+            break
+        }
+    }
+    return loser.score * die.rollCount
+}
 
 fun partTwo(input: String) = input.trim().length
--- src/test/kotlin/day21_dirac_dice/DiracDiceKtTest.kt
+++ src/test/kotlin/day21_dirac_dice/DiracDiceKtTest.kt
@@ -4,14 +4,15 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 private val WORKED_EXAMPLE = """
-    input
+    Player 1 starting position: 4
+    Player 2 starting position: 8
 """.trimIndent()
 
 internal class DiracDiceKtTest {
 
     @Test
     fun partOne() {
-        assertEquals(0, partOne(WORKED_EXAMPLE))
+        assertEquals(739785, partOne(WORKED_EXAMPLE))
     }
 
     @kotlin.test.Ignore // todo: reinstate when ready!
