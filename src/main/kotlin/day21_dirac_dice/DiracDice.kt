83f687d01deb3e44ae6476e3a3860a9486fc572f
Barney Boisvert <bboisvert@armus.com>
2021-12-22 16:02:36 -0800

    skeleton

---
+++ src/main/kotlin/day21_dirac_dice/DiracDice.kt
@@ -0,0 +1,13 @@
+package day21_dirac_dice
+
+/**
+ * todo: add notes
+ */
+fun main() {
+    util.solve(::partOne)
+//    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
