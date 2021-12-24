ca5658d6a6cc634c90e166e9a08d5f9bac7c7f37
Barney Boisvert <bboisvert@armus.com>
2021-12-23 16:01:09 -0800

    skeleton

---
+++ src/main/kotlin/day22_reactor_reboot/ReactorReboot.kt
@@ -0,0 +1,13 @@
+package day22_reactor_reboot
+
+/**
+ * todo: add notes
+ */
+fun main() {
+    util.solve(::partOne)
+//    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
---
+++ src/test/kotlin/day22_reactor_reboot/ReactorRebootKtTest.kt
@@ -0,0 +1,23 @@
+package day22_reactor_reboot
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+private val WORKED_EXAMPLE = """
+    input
+""".trimIndent()
+
+internal class ReactorRebootKtTest {
+
+    @Test
+    fun partOne() {
+        assertEquals(0, partOne(WORKED_EXAMPLE))
+    }
+
+    @kotlin.test.Ignore // todo: reinstate when ready!
+    @Test
+    fun partTwo() {
+        assertEquals(0, partTwo(WORKED_EXAMPLE))
+    }
+
+}
