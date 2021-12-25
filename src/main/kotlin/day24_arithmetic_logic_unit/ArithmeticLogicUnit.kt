f38ce8b5957dd970bdabcbb02bb5bffb351dbabc
Barney Boisvert <bboisvert@armus.com>
2021-12-25 11:52:45 -0800

    skeleton

---
+++ src/main/kotlin/day24_arithmetic_logic_unit/ArithmeticLogicUnit.kt
@@ -0,0 +1,13 @@
+package day24_arithmetic_logic_unit
+
+/**
+ * todo: add notes
+ */
+fun main() {
+    util.solve(::partOne)
+//    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
