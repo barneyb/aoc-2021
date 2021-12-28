369fc61dbcf76d3d4a3a70dee67d3f9a5d3c6333
Barney Boisvert <bboisvert@armus.com>
2021-12-28 11:42:22 -0800

    allow solve both parts separately

--- src/main/kotlin/day24_arithmetic_logic_unit/ArithmeticLogicUnit.kt
+++ src/main/kotlin/day24_arithmetic_logic_unit/ArithmeticLogicUnit.kt
@@ -4,7 +4,7 @@ package day24_arithmetic_logic_unit
  * todo: add notes
  */
 fun main() {
-//    util.solve(51983999947999, ::partOne) // 51993999947999 is too high
+    util.solve(51983999947999, ::partOne) // 51993999947999 is too high
     util.solve(11211791111365, ::partTwo)
 }
 
@@ -69,7 +69,7 @@ private fun findWs(
     next: MutableSet<Long>
 ) {
     for (w in 9 downTo 1L) {
-        val v = doRound(z, A[r], B[r], C[r], w)
+        val v = doRound(z, r, w)
         if (targets.contains(v)) {
             next.add(z)
 //            println("r=$r, z=$z, w=$w yields $v")
@@ -77,8 +77,7 @@ private fun findWs(
     }
 }
 
-fun partOne(program: String): Long {
-    // first back-track and find all the per-round output Z that might work
+private fun buildInputZByRound() {
     var targets = setOf(0L)
     for (r in 13 downTo 0) {
         if (targets.isEmpty()) {
@@ -87,6 +86,14 @@ fun partOne(program: String): Long {
         }
         targets = searchIn(r, targets)
     }
+}
+
+fun partOne(input: String) =
+    solve(input, 9 downTo 1L)
+
+fun solve(input: String, digitPriority: Iterable<Long>): Long {
+    // first back-track and find all the per-round output Z that might work
+    buildInputZByRound()
 
     // now go forward and find the largest per-round W that passes the check
     fun walk(r: Int, z: Long, digits: List<Long>): Long {
@@ -97,7 +104,7 @@ fun partOne(program: String): Long {
         }
         val targets = if (r == 13) setOf(0L)
         else inputZByRound[r + 1]!!
-        for (w in 1L..9) {
+        for (w in digitPriority) {
             val v = doRound(z, r, w)
             if (targets.contains(v)) {
                 val answer = walk(r + 1, v, digits + w)
@@ -111,4 +118,5 @@ fun partOne(program: String): Long {
     return walk(0, 0, emptyList())
 }
 
-fun partTwo(input: String) = partOne(input)
+fun partTwo(input: String) =
+    solve(input, 1..9L)
