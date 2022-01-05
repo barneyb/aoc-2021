1b0bc3d9165541dd5b6c19a5038a6399a3470c1c
Barney Boisvert <bboisvert@armus.com>
2022-01-05 14:58:54 -0800

    skeleton

---
+++ src/main/kotlin/day25_sea_cucumber/SeaCucumber.kt
@@ -0,0 +1,13 @@
+package day25_sea_cucumber
+
+/**
+ * todo: add notes
+ */
+fun main() {
+    util.solve(::partOne)
+//    util.solve(::partTwo)
+}
+
+fun partOne(input: String) = input.length
+
+fun partTwo(input: String) = input.trim().length
